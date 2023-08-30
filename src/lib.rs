1f5a6890a725890bcfc02034c086d526c3bf28cc
Shashank <kiwimojito3012@proton.me>
2023-08-30 10:28:40 +0530

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,213 +1,137 @@
-use std::net::UdpSocket;
-use std::net::{Ipv4Addr, Ipv6Addr};
+use std::error;
 
-type Error = Box<dyn std::error::Error>;
-type Result<T> = std::result::Result<T, Error>;
+type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 
-pub struct PacketBuf {
+//UDP packet sized 512 that will be written to and used for communication
+pub struct BytePacket {
     pub buf: [u8; 512],
-    pub pos: usize,
+    pos: usize,
 }
 
-impl PacketBuf {
-    pub fn new() -> PacketBuf {
-        PacketBuf {
+impl BytePacket {
+    pub fn new() -> Self {
+        Self {
             buf: [0; 512],
-            pos: 0,
+            pos: 0
         }
+ 
+ 
     }
 
-    fn pos(&self) -> usize {
+    pub fn pos(&self) -> usize {
         self.pos
     }
 
-    fn step(&mut self, steps: usize) -> Result<()> {
-        self.pos += steps;
-
-        Ok(())
-    }
-
-    fn seek(&mut self, pos: usize) -> Result<()> {
+    pub fn seek(&mut self, pos: usize) {
         self.pos = pos;
-
-        Ok(())
     }
 
-    fn read(&mut self) -> Result<u8> {
+    fn read_u8(&mut self) -> Result<u8> {
         if self.pos >= 512 {
-            return Err("invalid offset into buffer".into());
+            return Err("Read beyond bounds".into());
         }
+
         let res = self.buf[self.pos];
         self.pos += 1;
 
         Ok(res)
     }
 
-    fn get(&mut self, pos: usize) -> Result<u8> {
-        if pos >= 512 {
-            return Err("invalid offset into buffer".into());
+    fn read_u16(&mut self) -> Result<u16> {
+        
+        let res = (self.read_u8()? as u16) << 8 | self.read_u8()? as u16 ; //treat the read byte as u16, shift eight bits left i.e. add 8 zeroes and AND with second read byte
+
+        Ok(res)
+    }
+
+    //retrieve a byte without changing pos in buffer -> eg. to retrieve label in domain name using pointer
+    fn get_u8(&self, pos: usize) -> Result<u8> {
+        if pos > 512 {
+            return Err("Read beyond bounds".into());
         }
+
         Ok(self.buf[pos])
     }
 
-    fn get_range(&mut self, start: usize, len: usize) -> Result<&[u8]> {
+    fn get_range(&self, start: usize, len: usize) -> Result<&[u8]> {
         if start + len >= 512 {
-            return Err("invalid offset into buffer".into());
+            return Err("Read beyond bounds".into());
         }
-        Ok(&self.buf[start..start + len as usize])
+        
+        Ok(&self.buf[start..start + len])
     }
 
-    fn read_u16(&mut self) -> Result<u16> {
-        let res = ((self.read()? as u16) << 8) | (self.read()? as u16);
+    fn write_u8(&mut self, pos: usize, byte: u8) -> Result<()> {
+        if pos > 512 {
+            return Err("Write beyond bounds".into());
+        }
 
-        Ok(res)
+        self.buf[pos] = byte;
+        Ok(())
     }
 
-    fn read_u32(&mut self) -> Result<u32> {
-        let res = ((self.read()? as u32) << 24)
-            | ((self.read()? as u32) << 16)
-            | ((self.read()? as u32) << 8)
-            | ((self.read()? as u32) << 0);
+    // [3]www[6]google[3]com[0] -> labels
+    fn read_qname(&mut self) -> Result<String> { 
+        let mut delim =  " ";
 
-        Ok(res)
-    }
-
-    fn read_query_name(&mut self, output_str: &mut String) -> Result<()> {
-        let mut pos = self.pos();
         let mut jumped = false;
-
-        let mut delim = "";
-        let max_jumps = 5;
+        let mut jumps_max = 5;
         let mut jumps_performed = 0;
+
+        let mut pos = self.pos;
+
+        let mut domain_name: String = String::new();
+
         loop {
-           
-            if jumps_performed > max_jumps {
-                return Err(format!("Max limit of {} jumps exceeded", max_jumps).into());
+            if jumps_performed > jumps_max {
+                return Err("Jumped beyond max limit".into());
             }
 
-            let len = self.get(pos)?;
+            let len_byte = self.get_u8(self.pos)?;
 
-            
-            if (len & 0xC0) == 0xC0 {
-              
+            //check if the length has its 2 MSB set
+            if (len_byte & 0xC0) == 0xC0 {
                 if !jumped {
-                    self.seek(pos + 2)?;
-                }
+                    self.seek(pos + 2); //buf's pos updated
+                }   
 
-                let b2 = self.get(pos + 1)? as u16;
-                let offset = (((len as u16) ^ 0xC0) << 8) | b2;
-                pos = offset as usize;
+                let byte2 = self.get_u8(pos + 1)? as u16;
+                //to get offset, remove the 2 MSBs and take the two bytes together
+                let jump_offset = ( (len_byte ^ 0xC0) as u16 ) << 8 | byte2;
+
+                pos = jump_offset as usize;
                 jumped = true;
                 jumps_performed += 1;
+
                 continue;
             }
 
-            pos += 1;
+            // if not jump condition
+            else {
+                
+                if len_byte == 0 {
+                    break;
+                }
 
-            
-            if len == 0 {
-                break;
-            }
+                pos += 1;
 
-            output_str.push_str(delim);
+                domain_name.push_str(delim);
 
-            let str_buffer = self.get_range(pos, len as usize)?;
-            output_str.push_str(&String::from_utf8_lossy(str_buffer).to_lowercase());
+                let buffer = self.get_range(pos, len_byte as usize)?;
+                //convert this slice of bytes to string
+                domain_name.push_str(&String::from_utf8_lossy(buffer).to_lowercase());
 
-            delim = ".";
+                delim = ".";
 
-            pos += len as usize;
+                pos += len_byte as usize;
+            }   
         }
 
         if !jumped {
-            self.seek(pos)?;
+            self.seek(pos);
         }
 
-        Ok(())
-    }
-
-    fn write(&mut self, val: u8) -> Result<()> {
-        if self.pos >= 512 {
-            return Err("invalid offset into buffer".into());
-        }
-        self.buf[self.pos] = val;
-        self.pos += 1;
-        Ok(())
-    }
-
-    fn write_u8(&mut self, val: u8) -> Result<()> {
-        self.write(val)?;
-
-        Ok(())
-    }
-
-    fn write_u16(&mut self, val: u16) -> Result<()> {
-        self.write((val >> 8) as u8)?;
-        self.write((val & 0xFF) as u8)?;
-
-        Ok(())
-    }
-
-    fn write_u32(&mut self, val: u32) -> Result<()> {
-        self.write(((val >> 24) & 0xFF) as u8)?;
-        self.write(((val >> 16) & 0xFF) as u8)?;
-        self.write(((val >> 8) & 0xFF) as u8)?;
-        self.write(((val >> 0) & 0xFF) as u8)?;
-
-        Ok(())
-    }
-
-    fn write_query_name(&mut self, query_name: &str) -> Result<()> {
-        for label in query_name.split('.') {
-            let len = label.len();
-            if len > 0x34 {
-                return Err("Single label exceeds 63 characters of length".into());
-            }
-
-            self.write_u8(len as u8)?;
-            for b in label.as_bytes() {
-                self.write_u8(*b)?;
-            }
-        }
-
-        self.write_u8(0)?;
-
-        Ok(())
-    }
-
-    fn set(&mut self, pos: usize, val: u8) -> Result<()> {
-        self.buf[pos] = val;
-
-        Ok(())
-    }
-
-    fn set_u16(&mut self, pos: usize, val: u16) -> Result<()> {
-        self.set(pos, (val >> 8) as u8)?;
-        self.set(pos + 1, (val & 0xFF) as u8)?;
-
-        Ok(())
-    }
+        Ok(domain_name)
+    }   
 }
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
-pub enum ResultCode {
-    NOERROR = 0,
-    FORMERR = 1,
-    SERVFAIL = 2,
-    NXDOMAIN = 3,
-    NOTIMP = 4,
-    REFUSED = 5,
-}
-
-impl ResultCode {
-    pub fn from_num(num: u8) -> ResultCode {
-        match num {
-            1 => ResultCode::FORMERR,
-            2 => ResultCode::SERVFAIL,
-            3 => ResultCode::NXDOMAIN,
-            4 => ResultCode::NOTIMP,
-            5 => ResultCode::REFUSED,
-            0 | _ => ResultCode::NOERROR,
-        }
-    }
-}
